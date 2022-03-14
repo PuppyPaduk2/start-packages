3fef63898fe5be7d131d813ae441d061e41e1f76
puppypaduk2 <debraooclark@gmail.com>
2022-03-14 15:41:57 +0300

    feat: Add spawn

---
+++ util/spawn.js
@@ -0,0 +1,22 @@
+const { spawn: _spawn } = require("child_process");
+
+async function spawn(command, args, { onCreate, ...options } = {}) {
+  return new Promise((resolve) => {
+    const stdout = [];
+    const stderr = [];
+    const childProcess = spawn(command, args, options);
+
+    if (onCreate) onCreate(childProcess);
+
+    childProcess.stdout.on("data", (data) => stdout.push(data));
+    childProcess.stderr.on("data", (data) => stderr.push(data));
+    childProcess.on("close", (code) => resolve({ code, stdout, stderr }));
+  });
+}
+
+spawn.pipe = (childProcess) => {
+  childProcess.stdout.pipe(process.stdout);
+  childProcess.stderr.pipe(process.stderr);
+};
+
+module.exports = spawn;
